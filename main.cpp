2f251c57d972fafcd115a3aee5a46c216a9067cc
dflass <dflass@crimson.ua.edu>
2016-03-01 11:17:39 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include <QApplication>
+//#include <QtWidgets>
+
+
+
+//extern "C" int qtmn(int argc, char *argv[])
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("Dr. Jackson's Boat");
+
+    w.show();
+
+    return a.exec();
+
+
+}
